4268adb12c198e594b0c4cfe04dec2e0959d2b03
charlierobertsuk <charlie@robertsclan.co.uk>
2025-02-16 15:55:54 +0000

    chore: comment css and script code

--- script.js
+++ script.js
@@ -1,4 +1,5 @@
 class AlgorithmVisualiser {
+  // equivilant to python __init__
   constructor() {
     this.array = [];
     this.barsContainer = document.getElementById("bars-container");
@@ -7,8 +8,8 @@ class AlgorithmVisualiser {
 
   generateArray() {
     this.array = Array.from(
-      { length: 8 },
-      () => Math.floor(Math.random() * 99) + 1
+      { length: 8 }, // number of bars (there will be 8, 16, 32 variables)
+      () => Math.floor(Math.random() * 99) + 1 // random number between 1 and 100
     );
     this.renderBars();
   }
@@ -17,10 +18,10 @@ class AlgorithmVisualiser {
     this.array.forEach((value) => {
       const bar = document.createElement("div");
       bar.className = "sorting-bar";
-      bar.style.height = `${value * 2}px`;
+      bar.style.height = `${value * 2}px`; // 2 to 200px (height) in 100 incraments of 2px
       this.barsContainer.appendChild(bar);
     });
   }
 }
 
-window.addEventListener("load", () => new AlgorithmVisualiser());
+window.addEventListener("load", () => new AlgorithmVisualiser()); // loads visualiser
