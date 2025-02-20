34833e968eb0ae1f85e51e2902107bbd14a9993d
charlierobertsuk <charlie@robertsclan.co.uk>
2025-02-20 18:46:14 +0000

    feature: Array size button functionality

--- script.js
+++ script.js
@@ -2,19 +2,28 @@ class AlgorithmVisualiser {
   constructor() {
     this.array = [];
     this.barsContainer = document.getElementById("bars-container");
-    this.generateArray();
+    this.generateArray(16); // default array size - will change with screen size (mobile 8, default 16, wideish 32)
+
+    const sizeButtons = document.querySelectorAll(".btn-array-size");
+    sizeButtons.forEach((button) => {
+      button.addEventListener("click", () => {
+        const size = parseInt(button.getAttribute("data-array-size")); // gets the selected array size
+        this.generateArray(size); // regenerate the new array
+      });
+    });
   }
 
-  generateArray() {
+  generateArray(size) {
     const uniqueValues = new Set();
-    while (uniqueValues.size < 8) {
-      uniqueValues.add(Math.floor(Math.random() * 99) + 1); // random number between 1 and 100
+    while (uniqueValues.size < size) {
+      uniqueValues.add(Math.floor(Math.random() * 99) + 1); // random int between 1 and 100
     }
     this.array = Array.from(uniqueValues);
     this.renderBars();
   }
 
   renderBars() {
+    this.barsContainer.innerHTML = ""; // clear any current existing bars
     this.array.forEach((value) => {
       const bar = document.createElement("div");
       bar.className = "sorting-bar";
